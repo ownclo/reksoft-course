5b56a42c6eadb9336189f8151a4a10b5ad3c4e43
Ownclo - Arcis <mrownclo@gmail.com>
2014-03-15 19:32:17 +0400

    Implemented subscriptions and syncronous calls.

--- taskies/task.erl
+++ taskies/task.erl
@@ -10,8 +10,11 @@
 %% API
 -export([start/2
         ,start/1
+        ,start_wait/2
         ,stop/1
         ,get_status/1
+        ,subscribe/1
+        ,unsubscribe/1
         ,cancel/1
         ]).
 
@@ -32,14 +35,15 @@
 
 %% XXX: How can I guarantee the immutability of
 %% 'task_closure' and 'attempts_allowed'? NOWAY :(
-%% ReaderT TaskConfig State TaskStatus
+%% ReaderT TaskConfig (State TaskStatus)
 -record(state, {
-        status            :: task_status(),
-        worker            :: pid(),
+        status :: task_status(),
+        worker :: pid(),
+        subscribers = sets:new() :: sets:set(pid()),
 
         % IMMUTABLE
-        attempts_allowed  :: attempts_allowed(),
-        task_closure      :: task_closure()
+        attempts_allowed :: attempts_allowed(),
+        task_closure :: task_closure()
         }).
 
 %%%===================================================================
@@ -62,15 +66,39 @@ start(Task, Attempts) ->
 
 start(Task) -> start(Task, 1).
 
+% start and immediately subscribe to all messages
+% in one atomic step.
+start_subscribe(Task, Attempts) ->
+    gen_server:start(?MODULE, [Task, Attempts, {subscribe, self()}], []).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Start a task and wait for it to complete (or overflow the number
+%% of attempts).
+%% @end
+%%--------------------------------------------------------------------
+start_wait(Task, Attempts) ->
+    {ok, TID} = start_subscribe(Task, Attempts),
+    loop_wait(TID).
+
+cancel(TaskId) -> stop(TaskId).
+
 % will force the worker process to abort
 stop(TaskId) ->
     gen_server:cast(TaskId, stop).
 
-cancel(TaskId) -> stop(TaskId).
-
 -spec get_status(task_id()) -> task_status().
 get_status(TaskId) ->
     gen_server:call(TaskId, get_status).
+
+% XXX: Do need 'subscribe' be syncronous?
+% Probably yes, we want to guarantee that all
+% messages are delivered since subscription started.
+subscribe(TaskId) ->
+    gen_server:call(TaskId, {subscribe, self()}).
+
+unsubscribe(TaskId) ->
+    gen_server:cast(TaskId, {unsubscribe, self()}).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -96,7 +124,12 @@ init([Task, Attempts]) ->
                    task_closure=Task,
                    attempts_allowed=Attempts},
 
-    {ok, State}.
+    {ok, State};
+
+init([Task, Attempts, {subscribe, Pid}]) ->
+    {ok, State} = init([Task, Attempts]),
+    Subs = sets:add_element(Pid, State#state.subscribers),
+    {ok, State#state{subscribers = Subs}}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -113,6 +146,7 @@ init([Task, Attempts]) ->
 %% @end
 %%--------------------------------------------------------------------
 handle_call(get_status, _From, State) ->
+
     % NOTE: Stop ourselves if finishing is reported?
     % That is considered a poorer design decision.
     % While easily implementable, that behaviour will
@@ -131,6 +165,10 @@ handle_call(get_status, _From, State) ->
 
     {reply, State#state.status, State};
 
+handle_call({subscribe, Pid}, _From, State) ->
+    Subs = sets:add_element(Pid, State#state.subscribers),
+    {reply, ok, State#state{subscribers = Subs}};
+
 handle_call(_Request, _From, State) ->
     {reply, unsupported, State}.
 
@@ -146,8 +184,14 @@ handle_call(_Request, _From, State) ->
 %%--------------------------------------------------------------------
 
 handle_cast({result, Result}, State) ->
-    Status = {succeeded, Result},
-    {noreply, State#state{status=Status}};
+    NewState = State#state{status={succeeded, Result}},
+    notify_subscribers(NewState),
+    {noreply, NewState};
+
+handle_cast({unsubscribe, Pid}, State) ->
+    NewSubs = sets:del_element(Pid, State#state.subscribers),
+    {noreply, State#state{subscribers = NewSubs}};
+
 
 handle_cast(stop, State) ->
     {stop, normal, State};
@@ -191,6 +235,7 @@ handle_info({'EXIT', OldWorker, _Abnormal}, State) ->
         true ->  % Made >= Allowed
             State#state{status = fail_limit_reached}
     end,
+    notify_subscribers(NewState),
     {noreply, NewState};
 
 handle_info(_Info, State) ->
@@ -235,3 +280,22 @@ make_reporting(Pid, Task) ->
 
 report_result(Pid, Result) ->
     gen_server:cast(Pid, {result, Result}).
+
+notify_subscribers(State) ->
+    do_notify_subscribers(State#state.subscribers,
+                          {self(), State#state.status}).
+
+do_notify_subscribers(Subs, {TaskId, Status}) ->
+    Notify = fun(Sub) -> notify(Sub, {TaskId, Status}) end,
+    lists:map(Notify, sets:to_list(Subs)),
+    ok.
+
+notify(Sub, Status) ->
+    Sub ! Status.
+
+loop_wait(TID) ->
+    receive
+        {TID, {working, _}} -> loop_wait(TID);
+        {TID, fail_limit_reached} -> fail_limit_reached;
+        {TID, {succeeded, Res}} -> {succeeded, Res}
+    end.
--- taskies/usage.erl
+++ taskies/usage.erl
@@ -2,20 +2,43 @@
 -export([main/1]).
 -mode(compile).
 
+maybe_ok() ->
+    random:seed(erlang:now()),
+    Rnd = random:uniform(),
+    io:format("Number = ~p~n", [Rnd]),
+    if Rnd >= 0.5 ->
+            io:format("Dying...~n"),
+            exit(dead);
+        true -> io:format("Success...~n"), ok
+    end.
+
 main(_) ->
     _OkFunction = fun() -> io:format("Hi there!~n"), ok end,
     ExitFunction = fun() -> io:format("Dying...~n"), exit(dead) end,
 
-    {ok, TaskId} = task:start(ExitFunction, 10),
-    note("TaskId: ", TaskId),
+    {ok, TaskId} = task:start(fun maybe_ok/0, 10),
+    note("Subscription: ", task:subscribe(TaskId)),
+    % note("Unsubscription: ", task:unsubscribe(TaskId)),
 
-    Status = task:get_status(TaskId),
-    note("TaskStatus: ", Status),
+    note("TaskStatus: ", task:get_status(TaskId)),
 
     timer:sleep(5),
     note("TaskStatus: ", task:get_status(TaskId)),
+
     task:cancel(TaskId),
+    timer:sleep(10),  % for cancel message to arrive.
+    flush(),
+
+    io:format("~n~n~nSync Call...~n"),
+    note("Waiting call: ", task:start_wait(ExitFunction, 10)),
     ok.
+
+flush() ->
+    receive
+        M -> note("Mailbox: ", M),
+            flush()
+    after 0 -> ok
+    end.
 
 note(Note, Val) ->
     io:format("~s~p~n", [Note, Val]).
